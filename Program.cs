73cf4509939402b790aa47fcc2cfef2514c31645
Samir Medeiros <samir.medeiros@outlook.com>
2025-01-24 08:33:27 +0000

    Remover caracter especial e espaço da string

--- Program.cs
+++ Program.cs
@@ -1,10 +1,16 @@
-﻿namespace AppFuncoes
+﻿using System.Text.RegularExpressions;
+
+namespace AppFuncoes
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            // Remover caracter especial e espaço
+            string value_caracter_especial = "ABS_ rteste; &%";
+            Console.WriteLine(Regex.Replace(value_caracter_especial, "[^0-9a-zA-Z]+", ""));
+
         }
     }
 }
