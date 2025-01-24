2f62241f13c638395bdb4ba0f71a53b897318231
Samir Medeiros <samir.medeiros@outlook.com>
2025-01-24 08:35:51 +0000

    Converter string em DateTime

--- Program.cs
+++ Program.cs
@@ -11,6 +11,11 @@ namespace AppFuncoes
             string value_caracter_especial = "ABS_ rteste; &%";
             Console.WriteLine(Regex.Replace(value_caracter_especial, "[^0-9a-zA-Z]+", ""));
 
+            // Converter string em DateTime
+            string data_string = "20241204091516";
+            DateTime dataEmDateTime = DateTime.ParseExact(data_string, "yyyyMMddHHmmss", null);
+            Console.WriteLine(dataEmDateTime);
+
         }
     }
 }
